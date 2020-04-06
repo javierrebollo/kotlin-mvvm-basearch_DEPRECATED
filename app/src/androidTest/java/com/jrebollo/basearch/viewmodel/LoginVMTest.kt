a0a1556cd09acb0e300e1e90fa8ac19de5bd24a4
Javier Rebollo <javierrebollocinta@gmail.com>
2020-04-06 08:33:38 +0200

    Remove unnecessary comments

--- app/src/androidTest/java/com/jrebollo/basearch/viewmodel/LoginVMTest.kt
+++ app/src/androidTest/java/com/jrebollo/basearch/viewmodel/LoginVMTest.kt
@@ -17,11 +17,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class LoginVMTest {
     private lateinit var userRepository: UserRepository
     private lateinit var loginVM: LoginVM
